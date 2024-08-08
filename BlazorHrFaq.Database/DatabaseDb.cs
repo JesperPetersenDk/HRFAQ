df67eb8b231a645910e55702d179af688c2a941e
Jesper Petersen <info@ordklar.dk>
2024-08-08 23:46:55 +0200

    DatabaseDb + DbContext add now. Need UseSqlServer to databasDb.cs file,.

--- BlazorHrFaq.Database/DatabaseDb.cs
+++ BlazorHrFaq.Database/DatabaseDb.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BlazorHrFaq.Database.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorHrFaq.Database
 {
     public class DatabaseDb : DbContext
     {
+        public DbSet<Faq> Faq { get; set; }
 
+        public DatabaseDb()
+        {
+
+        }
+
+        public DatabaseDb(DbContextOptions<DatabaseDb> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("");
+            }
+        }
     }
 }
