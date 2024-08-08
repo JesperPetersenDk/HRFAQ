153e78652bca84570b657dd1c1535a02c7f8dff6
Jesper Petersen <info@ordklar.dk>
2024-08-08 23:44:10 +0200

    Design of Faq database + Ready to set up the Database.

---
+++ BlazorHrFaq.Database/DatabaseDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorHrFaq.Database
+{
+    public class DatabaseDb : DbContext
+    {
+
+    }
+}
---
+++ BlazorHrFaq.Database/Model/Faq.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BlazorHrFaq.Database.Model
+{
+    public class Faq
+    {
+        [Key]
+        public Guid FaqId { get; set; }
+        public string SearchWords { get; set; }
+        public string Answer { get; set; }
+        public DateTime? LastUpdate { get; set; }
+        public int? HitCount { get; set; } = 0;
+        public int? Priority { get; set; } = 5;
+
+    }
+}
