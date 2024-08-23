9fa0dba4ab61bce189b37c7f39b4c4dace9deb4c
Jesper Petersen <info@ordklar.dk>
2024-08-23 23:44:25 +0200

    Add match data + Get match word + Extensions ready for PDF and Images + Update Commands.

--- BlazorHrFaq.Database.Infrastructure/Commands.cs
+++ BlazorHrFaq.Database.Infrastructure/Commands.cs
@@ -1,14 +1,38 @@
 ﻿using BlazorHrFaq.Database.Model;
+using Extensions;
 using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BlazorHrFaq.Database.Infrastructure
 {
     public interface ICommands
     {
         Task<List<Faq>> GetFaq(string text);
+        Task<Tuple<int, string>> AddMatchData(string text, string value);
+        Task<List<MatchData>> GetMatchData();
     }
     public class Commands : ICommands
     {
+        public async Task<Tuple<int, string>> AddMatchData(string text, string value)
+        {
+            using (var db = new DatabaseDb())
+            {
+                int saveInDatabase = 0; // False to save in database
+                string codeValue = Extensions.Extensions.FormatFileType(value);
+                MatchData matchData = new MatchData
+                {
+                    CodeValue = codeValue,
+                    Value = value,
+                    Text = text
+                };
+
+                await db.Match.AddAsync(matchData);
+                saveInDatabase = await db.SaveChangesAsync();
+
+                return Tuple.Create(saveInDatabase, codeValue);
+            }
+        }
+
         public async Task<List<Faq>> GetFaq(string text)
         {
             using(var db = new DatabaseDb())
@@ -27,6 +51,14 @@ namespace BlazorHrFaq.Database.Infrastructure
                     return resultData;
                 }
                 return null;                
+            }
+        }
+
+        public async Task<List<MatchData>> GetMatchData()
+        {
+            using (var db = new DatabaseDb())
+            {
+                return await db.Match.OrderByDescending(r => r.MatchId).ToListAsync();
             }
         }
     }
---
+++ ViewModel/MatchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHrFaq.Faq.Model
+{
+    public class MatchViewModel
+    {
+        public string Code { get; set; } = string.Empty;
+        public List<MatchListViewModel> data { get; set; }
+    }
+
+    public class MatchListViewModel
+    {
+        public string Value { get; set; } //Url to site, image url or other.
+        public string Text { get; set; } //Text to link, value to image or other.
+        public string CodeValue { get; set; } // Random code value to user
+    }
+}
