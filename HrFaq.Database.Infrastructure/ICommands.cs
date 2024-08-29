87821f51c1539200d43623b524c2b010426dc490
Jesper Petersen <info@ordklar.dk>
2024-08-30 00:44:15 +0200

    Create Interface to command (single file)

---
+++ HrFaq.Database.Infrastructure/ICommands.cs
@@ -0,0 +1,20 @@
+﻿using HrFaq.Settings.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrFaq.Database.Infrastructure
+{
+    public interface ICommands
+    {
+        Task<List<Tuple<string>>> GetFaq();
+        Task<List<Tuple<string, int>>> GetFaq(string text);
+        Task<bool> CreateFaq(string searchwords, string answer);
+        Task<Tuple<int, string>> AddMatchData(string text, string value);
+        Task<List<Tuple<string, string, string>>> GetMatchData();
+        Task<List<SettingModel>> SettingInformation();
+        Task<bool> SaveSettingInfo(SettingModel model);
+    }
+}
