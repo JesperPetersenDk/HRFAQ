39f2729393878d12f86d1eed4dfed364a87cf5fe
Jesper Petersen <info@ordklar.dk>
2024-08-30 15:58:07 +0200

    Move Test file to same project folder/files

--- HrFaq.Faq.Xunit.Test/Test_Loggning_Service.cs
+++ HrFaq.Faq.Xunit.Test/Test_Loggning_Service.cs
@@ -1,6 +1,6 @@
 namespace HRFAQ.Loggning.Xunit.Test
 {
-    public class UnitTest1
+    public class Test_Loggning_Service
     {
         [Fact]
         public void Test1()
---
+++ HrFaq.Loggning.Xunit.Test/Test_Loggning_Service.cs
@@ -0,0 +1,11 @@
+namespace HRFAQ.Loggning.Xunit.Test
+{
+    public class Test_Loggning_Service
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
